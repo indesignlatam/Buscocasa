2607f5b589c44882aca554e2faadd4dcb082204a
Paulo Mogollon <wanchopeblanco@gmail.com>
2015-08-04 19:59:06 -0500

    Featured ribbon test

--- resources/views/listings/list.blade.php
+++ resources/views/listings/list.blade.php
@@ -2,9 +2,7 @@
 	<!-- Tags start -->
 	@if($listing->featuredType && $listing->featured_expires_at > Carbon::now())
 		<div class="uk-panel uk-panel-box uk-panel-box-primary uk-margin-remove">
-		@if($listing->featuredType->id > 1)
 			<img src="{{asset($listing->featuredType->image_path)}}" style="position:absolute; top:0; left:0; max-width:150px">
-		@endif
 	@else
 		<div class="uk-panel uk-panel-hover uk-margin-remove">
 			@if(Carbon::createFromFormat('Y-m-d H:i:s', $listing->created_at)->diffInDays(Carbon::now()) < 5)
