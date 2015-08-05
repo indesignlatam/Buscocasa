1cbc0dd4e551be9c1f5511834c1bd9e51b80e9b8
Paulo Mogollon <wanchopeblanco@gmail.com>
2015-08-05 10:10:23 -0500

    Added import - Tokenmismatch exeption must redirect to previous. #216

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,6 +1,7 @@
 <?php namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Session\TokenMismatchException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler {
