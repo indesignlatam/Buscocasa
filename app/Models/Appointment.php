5e13688289a434be081abf9bc56d791b963ec57f
Paulo Mogollon <wanchopeblanco@gmail.com>
2015-08-07 09:11:37 -0500

    Code audit #228
    ## Appointment
    * Removed soft delete
    * Editrules = rules
    * Ordered fillable
    * Hidden is now default on Indesign models
    * Removed unused appends
    * Added comments

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -3,8 +3,6 @@
 use App\Models\IndesignModel;
 
 class Appointment extends IndesignModel {
-	//
-    protected $softDelete = false;
 
     /**
      * The name of the table.
@@ -39,11 +37,11 @@ class Appointment extends IndesignModel {
      * @var array
      */
     protected $editRules    = [ 'user_id'       => 'numeric|exists:users,id',
-                                'listing_id'    => 'numeric|exists:listings,id',
-                                'name'          => 'string|max:255',
-                                'email'         => 'email',
-                                'phone'         => 'string|max:15|min:7',
-                                'comments'      => 'string|max:500',
+                                'listing_id'    => 'required|numeric|exists:listings,id',
+                                'name'          => 'required|string|max:255',
+                                'email'         => 'required|email',
+                                'phone'         => 'required|digits_between:7,15',
+                                'comments'      => 'required|string|max:500',
                                 ];
 
     /**
@@ -51,36 +49,17 @@ class Appointment extends IndesignModel {
      *
      * @var array
      */
-    protected $fillable = ['user_id', 'listing_id','name', 'email', 'phone', 'comments'];
+    protected $fillable = [ 'user_id', 
+                            'listing_id', 
+                            'name', 
+                            'email', 
+                            'phone', 
+                            'comments',
+                            ];
+
 
     /**
-     * The attributes that are hidden to JSON responces.
-     *
-     * @var array
-     */
-    protected $hidden = ['created_at', 'deleted_at'];
-
-    /**
-     * The attributes that are appended to JSON responces.
-     *
-     * @var array
-     */
-    //protected $appends = ['image_url'];
-
-    /**
-     * The method that appends the attribute to JSON responces.
-     *
-     * @var null or attribute
-     */
-    // public function getImageUrlAttribute(){
-    //  if($this->image_path){
-    //      return asset($this->image_path);
-    //  }
-    //  return null;
-    // }
-
-    /**
-     * Scope a query to only include non expired listings.
+     * Scope a query to only include unread messages
      *
      * @return \Illuminate\Database\Eloquent\Builder
      */
@@ -88,16 +67,30 @@ class Appointment extends IndesignModel {
         return $query->where('read', false);
     }
 
+    /**
+     * Scope a query to only include unanswered messages
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
     public function scopeNotAnswered($query){
         return $query->where('answered', false);
     }
 
 
-
+    /**
+     * Relationship with user to whom the message belongs
+     *
+     * @return \App\User
+     */
     public function user(){
         return $this->belongsTo('App\User', 'user_id');
     }
 
+    /**
+     * Relationship with listing which the message belongs to
+     *
+     * @return \App\Models\Listing
+     */
     public function listing(){
         return $this->belongsTo('App\Models\Listing', 'listing_id');
     }
