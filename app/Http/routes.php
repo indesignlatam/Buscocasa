d4bf5beff77e41ed7492cee37d24dca55bc473c6
Paulo Mogollon <wanchopeblanco@gmail.com>
2015-08-07 08:48:36 -0500

    Reorganised routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,16 +16,128 @@ setlocale(LC_ALL, 'es_ES');
 setlocale(LC_MONETARY, 'en_US');
 date_default_timezone_set('America/Bogota');
 
+/*
+|--------------------------------------------------------------------------
+| Admin Routes
+|--------------------------------------------------------------------------
+|
+| Here are all the API routes for external consumption.
+|
+| 
+|
+*/
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
+	Route::get('/', 'HomeController@index');
 
-Route::get('/', 'WelcomeController@index');
+	Route::get('listings/limit', 'ListingController@limitShow');// Secured
+	Route::get('listings/{id}/renovate', 'ListingController@renovateShow');// Secured
+	Route::post('listings/{id}/renovate', 'ListingController@renovate');// Secured
+	Route::get('listings/{id}/recover', 'ListingController@recover');// Secured
+	Route::resource('listings', 'ListingController', ['only' => ['index', 'create', 'store', 'edit', 'update', 'destroy']]);// Secured
 
+	Route::resource('images', 'ImageController', ['only' => ['store', 'destroy']]);// Secured
+
+	Route::post('messages/{id}/answer', 'AppointmentController@answer');// Secured
+	Route::post('messages/{id}/mark', 'AppointmentController@markAsRead');// Secured
+	Route::resource('messages', 'AppointmentController', ['only' => ['index', 'store', 'show','destroy']]);// Secured
+
+	Route::resource('destacar', 'HighlightController');
+
+	Route::delete('pagos/{id}', 'PaymentController@cancel');// Secured
+	Route::get('pagos/respuesta', 'PaymentController@payUResponse');
+	Route::resource('pagos', 'PaymentController');
+
+	Route::resource('banners', 'BannerController');
+
+	Route::get('user/send_confirmation_email', 'UserController@sendConfirmationEmail');// Secured
+	Route::get('user/not_confirmed', 'UserController@notConfirmed');// Secured
+	Route::resource('user', 'UserController', ['only' => ['edit', 'update']]);// Secured
+});
+
+/*
+|--------------------------------------------------------------------------
+| Super user admin Routes
+|--------------------------------------------------------------------------
+|
+| Here are all the API routes for external consumption.
+|
+| 
+|
+*/
+Route::group(['prefix' => 'admin', 'middleware' => 'auth.admin'], function(){
+	Route::resource('config', 'SettingsController');
+	Route::resource('users', 'UserController');
+
+	Route::resource('categories', 'CategoryController');
+	Route::resource('feature-categories', 'FeatureCategoryController');
+	Route::resource('listing-types', 'ListingTypeController');
+	Route::resource('listing-statuses', 'ListingStatusController');
+
+	Route::resource('features', 'FeatureController');
+	Route::resource('cities', 'CityController');
+
+	Route::resource('roles', 'RoleController');
+	Route::post('roles/attach', 'RoleController@attachPermission');
+	Route::post('roles/delete', 'RoleController@destroyMultiple');
+	Route::resource('permissions', 'PermissionController');
+	Route::post('permissions/delete', 'PermissionController@destroyMultiple');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Frontend Routes
+|--------------------------------------------------------------------------
+|
+| Here are all the API routes for external consumption.
+|
+| 
+|
+*/
 Route::controllers([
 	'auth' 		=> 'Auth\AuthController',
 	'password' 	=> 'Auth\PasswordController',
 ]);
-
 Route::get('social-auth/{provider?}', 'Auth\AuthController@redirectToProvider');
 Route::get('social-auth/{provider?}/redirects', 'Auth\AuthController@handleProviderCallback');
+
+
+Route::get('/', 'WelcomeController@index');
+
+Route::resource('ventas', 'ListingFEController');
+Route::resource('arriendos', 'ListingFEController');
+Route::resource('buscar', 'ListingFEController', ['only' => ['index']]);
+
+Route::post('pagos/confirmar', 'PaymentController@confirm');
+Route::post('pagos/disputas', 'PaymentController@dispute');
+
+Route::get('user/{id}/confirm/{code}', 'UserController@confirm');
+Route::get('/{username}', 'UserController@show');
+
+Route::post('appointments', 'AppointmentController@store');
+
+Route::resource('nosotros', 'FrontendController@nosotros');
+Route::resource('publicar', 'FrontendController@publica');
+
+/*
+|--------------------------------------------------------------------------
+| Cookie Routes
+|--------------------------------------------------------------------------
+|
+| Here are all the API routes for external consumption.
+|
+| 
+|
+*/
+Route::group(['prefix' => 'cookie'], function(){
+	Route::post('/set', function(){
+		if(Input::has('time')){
+			Cookie::queue(Input::get('key'), Input::get('value'), Input::get('time'));
+		}else{
+			Cookie::queue(Input::get('key'), Input::get('value'));
+		}
+	    return Response::json(['success' => true, 'key' => Input::get('key'), 'value' => Input::get('value')]);
+	});
+});
 
 /*
 |--------------------------------------------------------------------------
@@ -67,106 +179,3 @@ Route::group(['prefix' => 'emails', 'middleware' => 'auth.admin'], function(){
 	    return view('emails.tips', ['listing' => Auth::user()->listings->first()]);
 	});
 });
-
-/*
-|--------------------------------------------------------------------------
-| Cookie Routes
-|--------------------------------------------------------------------------
-|
-| Here are all the API routes for external consumption.
-|
-| 
-|
-*/
-Route::group(['prefix' => 'cookie'], function(){
-	Route::post('/set', function(){
-		if(Input::has('time')){
-			Cookie::queue(Input::get('key'), Input::get('value'), Input::get('time'));
-		}else{
-			Cookie::queue(Input::get('key'), Input::get('value'));
-		}
-	    return Response::json(['success' => true, 'key' => Input::get('key'), 'value' => Input::get('value')]);
-	});
-});
-/*
-|--------------------------------------------------------------------------
-| Admin Routes
-|--------------------------------------------------------------------------
-|
-| Here are all the API routes for external consumption.
-|
-| 
-|
-*/
-Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
-	Route::get('/', 'HomeController@index');
-	Route::get('/home', 'HomeController@index');
-
-	Route::get('listings/limit', 'ListingController@limitShow');// Secured
-	Route::get('listings/{id}/renovate', 'ListingController@renovateShow');// Secured
-	Route::post('listings/{id}/renovate', 'ListingController@renovate');// Secured
-	Route::get('listings/{id}/recover', 'ListingController@recover');// Secured
-	Route::resource('listings', 'ListingController', ['only' => ['index', 'create', 'store', 'edit', 'update', 'destroy']]);// Secured
-
-	Route::resource('images', 'ImageController', ['only' => ['store', 'destroy']]);// Secured
-
-	Route::post('messages/{id}/answer', 'AppointmentController@answer');// Secured
-	Route::post('messages/{id}/mark', 'AppointmentController@markAsRead');// Secured
-	Route::resource('messages', 'AppointmentController', ['only' => ['index', 'store', 'show','destroy']]);// Secured
-
-	Route::resource('destacar', 'HighlightController');
-
-	Route::delete('pagos/{id}', 'PaymentController@cancel');// Secured
-	Route::get('pagos/respuesta', 'PaymentController@payUResponse');
-	Route::resource('pagos', 'PaymentController');
-
-	Route::resource('banners', 'BannerController');
-
-	Route::get('user/send_confirmation_email', 'UserController@sendConfirmationEmail');// Secured
-	Route::get('user/not_confirmed', 'UserController@notConfirmed');// Secured
-	Route::resource('user', 'UserController', ['only' => ['edit', 'update']]);// Secured
-});
-
-Route::group(['prefix' => 'admin', 'middleware' => 'auth.admin'], function(){
-	Route::resource('config', 'SettingsController');
-	Route::resource('users', 'UserController');
-
-	Route::resource('categories', 'CategoryController');
-	Route::resource('feature-categories', 'FeatureCategoryController');
-	Route::resource('listing-types', 'ListingTypeController');
-	Route::resource('listing-statuses', 'ListingStatusController');
-
-	Route::resource('features', 'FeatureController');
-	Route::resource('cities', 'CityController');
-
-	Route::resource('roles', 'RoleController');
-	Route::post('roles/attach', 'RoleController@attachPermission');
-	Route::post('roles/delete', 'RoleController@destroyMultiple');
-	Route::resource('permissions', 'PermissionController');
-	Route::post('permissions/delete', 'PermissionController@destroyMultiple');
-});
-
-/*
-|--------------------------------------------------------------------------
-| Frontend Routes
-|--------------------------------------------------------------------------
-|
-| Here are all the API routes for external consumption.
-|
-| 
-|
-*/
-Route::resource('ventas', 'ListingFEController');
-Route::resource('arriendos', 'ListingFEController');
-Route::resource('buscar', 'ListingFEController', ['only' => ['index']]);
-
-Route::post('pagos/confirmar', 'PaymentController@confirm');
-Route::post('pagos/disputas', 'PaymentController@dispute');
-
-Route::get('user/{id}/confirm/{code}', 'UserController@confirm');
-Route::get('/{username}', 'UserController@show');
-
-Route::post('appointments', 'AppointmentController@store');
-
-Route::resource('nosotros', 'FrontendController@nosotros');
-Route::resource('publicar', 'FrontendController@publica');
